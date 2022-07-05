51bbcc2a5341afd8dc121a36dcd3252179e4484b
Kirill <freshjs.ftd.@gmail.com>
2022-07-05 18:25:49 +0300

    Add mongoose CRUD

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,14 +1,87 @@
-const User = require('../db/models/user')
+const createHttpError = require('http-errors');
+const User = require('../db/models/user');
 
 module.exports.createUser = async (req, res, next) => {
   try {
-    const { body } = req
+    const { body } = req;
 
-    const user = await User.create(body)
+    const user = await User.create(body);
 
-    res.status(201).send({data: user})
-    
+    res.status(201).send({ data: user });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
+
+module.exports.getUsers = async (req, res, next) => {
+  try {
+    const {
+      query: { limit = 0, offset = 0 },
+    } = req;
+    const users = await User.find()
+      .select('-password -__v')
+      .limit(limit)
+      .skip(offset);
+
+    res.status(200).send({ data: users });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.getUser = async (req, res, next) => {
+  try {
+    const {
+      params: { userId },
+    } = req;
+
+    const user = await User.findById(userId, '-password');
+
+    if (!user) {
+      return next(createHttpError(404, 'User not found'));
+    }
+
+    res.status(200).send({ data: user });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.updateUser = async (req, res, next) => {
+  try {
+    const {
+      body,
+      params: { userId },
+    } = req;
+
+    const user = await User.findOneAndUpdate({ _id: userId }, body, {
+      new: true,
+    });
+
+    if (!user) {
+      return next(createHttpError(404, 'User not found'));
+    }
+
+    res.status(200).send({ data: user });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.deleteUser = async (req, res, next) => {
+  try {
+    const {
+      params: { userId },
+    } = req;
+
+    const user = await User.findByIdAndDelete(userId);
+
+    if (!user) {
+      return next(createHttpError(404, 'User not found'));
+    }
+
+    res.status(200).send({ data: user });
+  } catch (error) {
+    next(error);
+  }
+};
--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -1,6 +1,10 @@
-const userRouter = require('express').Router()
-const UserController = require('../controllers/user.controller')
-userRouter.post('/', UserController.createUser)
-// userRouter.get('/')
+const userRouter = require('express').Router();
+const UserController = require('../controllers/user.controller');
+userRouter.post('/', UserController.createUser);
+userRouter.get('/', UserController.getUsers);
 
-module.exports = userRouter
+userRouter.get('/:userId', UserController.getUser);
+userRouter.put('/:userId', UserController.updateUser);
+userRouter.delete('/:userId', UserController.deleteUser);
+
+module.exports = userRouter;
