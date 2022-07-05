7a79076efc9abbcffa0581c9a7b2792f8f75320d
Kirill <freshjs.ftd.@gmail.com>
2022-07-05 22:15:27 +0300

    Add cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const router = require('./routers')
+const express = require('express');
+const router = require('./routers');
+const cors = require('cors');
 
-const app = express()
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.use(express.json())
+app.use('/api', router);
 
-app.use('/api', router)
-
-module.exports = app
+module.exports = app;
